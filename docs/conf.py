d15c08387e7d28954597d518b0c118105f3b8592
M. Randy Aswin <mrandyaswin@gmail.com>
2025-06-12 14:52:38 +0800

    fix version

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath('../src'))
 
 project = 'geosquare-grid'
 author = 'Geosquare Team'
-release = '0.1.0'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 
